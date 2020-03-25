0e3b378a3fd951e3d12177f36021617bd15af02b
Timo <timogos.k@live.nl>
2020-03-25 19:57:55 +0100

    WIP Improve chart performance

--- chart/src/components/Trend.vue
+++ chart/src/components/Trend.vue
@@ -1,18 +1,48 @@
+<template>
+  <canvas ref="line-chart"></canvas>
+</template>
+
 <script>
-import { Line } from 'vue-chartjs'
+// import { Line } from 'vue-chartjs'
+
+// export default {
+//   props: ['data', 'options'],
+//   mixins: [Line],
+//   mounted () {
+//     this.renderChart(this.data, this.options)
+//   },
+//   watch: {
+//     data () {
+//       this.renderChart(this.data, this.options)
+//     }
+//   }
+// }
+
+import Chart from 'chart.js'
 
 export default {
   props: ['data', 'options'],
-  mixins: [Line],
   mounted () {
-    this.renderChart(this.data, this.options)
+    const ctx = this.$refs['line-chart'];
+    new Chart(ctx, {
+      type: 'line',
+      data: this.data,
+      options: this.options,
+    });
   },
   watch: {
     data () {
-      this.renderChart(this.data, this.options)
+      const ctx = this.$refs['line-chart'];
+      new Chart(ctx, {
+        type: 'line',
+        data: this.data,
+        options: this.options,
+      });
     }
   }
 }
+
+
 </script>
 
 <style>
